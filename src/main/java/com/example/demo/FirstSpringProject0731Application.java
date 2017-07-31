67a28e63f91b845a9deef1c5759c3d1f0545431c
yling0 <xyun2016@gmail.com>
2017-07-31 17:12:33 -0400

    first commit

---
+++ src/main/java/com/example/demo/FirstSpringProject0731Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringProject0731Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringProject0731Application.class, args);
+	}
+}
